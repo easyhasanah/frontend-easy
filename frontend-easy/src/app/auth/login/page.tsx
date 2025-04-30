3ca8b8d6e8890f248b3f2eed7c9609d82809f86b
8Fadhil <fadhil0803@gmail.com>
2025-04-30 11:36:17 +0700

    project next fix

---
+++ frontend-easy/src/app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import Login from "@/app/components/auth/Login";
+import { FC } from "react";
+
+const Page: FC = () => {
+  return (
+    <div>
+      <p>hasibcwejf</p>
+      <Login></Login>
+    </div>
+  );
+};
+
+export default Page;
