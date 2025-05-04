a1a445d28a159e3e43a65ee043e7b6fe605b6cbf
Muhammad Gibran Naufal Dzakwan <62495592+G1NDZ@users.noreply.github.com>
2025-05-04 20:29:36 +0700

    Update axios.ts

--- frontend-easy/src/lib/axios.ts
+++ frontend-easy/src/lib/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: 'http://192.168.23.50:8000/api',
   headers: {
-    "Content-Type": "application/json",
-  },
+    'Content-Type': 'application/json'
+  }
 });
 
 export default api;
